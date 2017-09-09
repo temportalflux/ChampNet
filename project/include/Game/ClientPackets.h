08697706f2b6bcc548b17a7ac10ff98e32e6cbea
Dustin Yost <dustin.yost@mymail.champlain.edu>
2017-09-09 14:21:57 -0400

    Add responses in Client for ID_NEW_CLIENT_JOINED and ID_CLIENT_NUMBER

--- project/include/Game/ClientPackets.h
+++ project/include/Game/ClientPackets.h
@@ -5,10 +5,12 @@
 namespace Game {
 
 	class ClientPackets : public Network::PacketHandler {
+		typedef unsigned int UserID;
 
 	private:
 		Network::Network* mpNetwork;
 		RakNet::SystemAddress mAddressServer;
+		UserID mID;
 
 	public:
 		ClientPackets();
--- project/source/Game/ClientPackets.cpp
+++ project/source/Game/ClientPackets.cpp
@@ -1,3 +1,14 @@
+/*
+	Dustin Yost 0984932
+	EGP-405-02
+	Project1
+	09/21/2017
+	We certify that this work is entirely our own.
+	The assessor of this project may reproduce this project and provide
+	copies to other academic staff, and/or communicate a copy of this project
+	to a plagiarism-checking service, which may retain a copy of
+	the project on its database.
+*/
 #include "Game\ClientPackets.h"
 
 #include "Network\Network.h"
@@ -5,6 +16,7 @@
 
 #include <iostream>
 
+// AUTHOR: Dustin Yost
 namespace Game {
 
 	ClientPackets::ClientPackets() {
@@ -18,6 +30,8 @@ namespace Game {
 	void ClientPackets::subscribeTo(Network::Network* network) {
 		this->mpNetwork = network;
 		network->addPacketHandler(ID_CONNECTION_REQUEST_ACCEPTED, this);
+		network->addPacketHandler(ID_NEW_CLIENT_JOINED, this);
+		network->addPacketHandler(ID_CLIENT_NUMBER, this);
 		//network->addPacketHandler(ID_NO_FREE_INCOMING_CONNECTIONS, this);
 		//network->addPacketHandler(ID_DISCONNECTION_NOTIFICATION, this);
 		//network->addPacketHandler(ID_CONNECTION_LOST, this);
@@ -64,9 +78,8 @@ namespace Game {
 		}
 		*/
 		switch (info.getPacketType()) {
-			case ID_CONNECTION_REQUEST_ACCEPTED:
+			case ID_CONNECTION_REQUEST_ACCEPTED: // Handle connections
 				{
-					// AUTHOR: Dustin Yost
 					this->mAddressServer = info.address;
 
 					// Construct response packet
@@ -79,6 +92,16 @@ namespace Game {
 					// Send username to server
 					this->mpNetwork->sendTo(packetUsername, this->mAddressServer, HIGH_PRIORITY, RELIABLE_ORDERED, 0, false);
 					
+				}
+				break;
+			case ID_NEW_CLIENT_JOINED: // Handle the message saying some client has joined
+				{
+					printf("> %s\n", ((PacketStringLarge*)info.data)->content);
+				}
+				break;
+			case ID_CLIENT_NUMBER: // Handle incoming assignment to the user id
+				{
+					mID = ((PacketUInt*)info.data)->clientID;
 				}
 				break;
 			default:
