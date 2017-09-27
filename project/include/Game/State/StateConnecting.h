e63a9facfdd51b45470b3633e5e641b05fa9047a
Dustin Yost <dustin.yost@mymail.champlain.edu>
2017-09-27 19:46:50 -0400

    Update documentation

--- project/include/Game/State/StateConnecting.h
+++ project/include/Game/State/StateConnecting.h
@@ -14,23 +14,29 @@ private:
 	// The address of the other player
 	NetAddress *mpAddressPeer;
 
+	/* Author: Dustin Yost
+	 * Handles switch from this state
+	 */
 	virtual void queueNextGameState() override;
 
 	virtual void onEnterFrom(StateApplication *previous);
 
+	/* Author: Dustin Yost
+	 * Update the network interface
+	 */
 	void updateNetwork();
 
 	/* Author: Dustin Yost
-		Handles all incoming packets - as BOTH a Host (Server) or Peer (Client)
-	*/
+	 * Handles all incoming packets - as BOTH a Host (Server) or Peer (Client)
+	 */
 	void handlePacket(PacketInfo *info);
 	/* Author: Dustin Yost
-		Handles all incoming packets - as a Host (Server)
-	*/
+	 * Handles all incoming packets - as a Host (Server)
+	 */
 	void handlePacketServer(PacketInfo *info);
 	/* Author: Dustin Yost
-		Handles all incoming packets - as a Peer (Client)
-	*/
+	 * Handles all incoming packets - as a Peer (Client)
+	 */
 	void handlePacketClient(PacketInfo *info);
 	void updateGame();
 	void render();
--- project/include/Game/State/StateGameNetwork.h
+++ project/include/Game/State/StateGameNetwork.h
@@ -9,24 +9,45 @@ struct NetAddress;
 class StateGameNetwork : public StateGame, public MessageHandler {
 
 private:
+	// The network interface to send packets through
 	Net *mpNetwork;
+	// The connect peer address
 	NetAddress *mpAddressPeer;
+	// The ID of the current player
 	PlayerIdentifier mPlayerID;
-	bool mIsPlayingAgain;
+	// If we are waiting for a move from the opponent
 	bool mIsWaitingForMove;
+	// If we are ready to play again
+	bool mIsPlayingAgain;
 
+	/* Author: Dustin Yost
+	 * Handles switch from this state, to either Connecting or Lobby states
+	 */
 	void queueNextGameState() override;
+
+	/* Author: Dustin Yost
+	 * Handles prep for when the state is exitting
+	 */
 	void onExit() override;
 
+	/* Author: Dustin Yost
+	 * Update the network interface
+	 */
 	void updateNetwork();
 
 	/* Author: Dustin Yost
-		Handles all incoming packets - as BOTH a Host (Server) or Peer (Client)
-	*/
+	 * Handles all incoming packets - as BOTH a Host (Server) or Peer (Client)
+	 */
 	void handlePacket(PacketInfo *info);
 
+	/* Author: Dustin Yost
+	 * Sends some packet ID to the connected peer
+	 */
 	void sendToPeer(unsigned char id);
 
+	/* Author: Dustin Yost
+	 * Return the opposite ID of mPlayerID
+	 */
 	PlayerIdentifier getInvertID();
 
 public:
@@ -35,16 +56,16 @@ public:
 	virtual ~StateGameNetwork();
 
 	/* Author: Dustin Yost
-		Sets the player's icon in a specfied slot,
-		and sets the next player.
-		Returns the player that won, NONE if no one has won yet.
-		Sends a packet to the peer that we have committed a move.
-	*/
+	 * Sets the player's icon in a specfied slot,
+	 * and sets the next player.
+	 * Returns the player that won, NONE if no one has won yet.
+	 * Sends a packet to the peer that we have committed a move.
+	 */
 	virtual PlayerIdentifier commitMove(int slot, PlayerIdentifier player);
 
 	/*
-	* Handles setting a wait flag when the user has committed their turn 
-	*/
+	 * Handles setting a wait flag when the user has committed their turn 
+	 */
 	void onMoveCommitted();
 
 };
