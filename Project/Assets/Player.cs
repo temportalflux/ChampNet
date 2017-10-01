1cc65429583eaef7425b4483acadf584e82314d7
dustin.yost <dustin.yost@mymail.champlain.edu>
2017-09-30 21:42:36 -0400

    Add unity project with input handling
    Former-commit-id: 69dfd2e944ef3a8c8f87f7b4c19ad604a14cd5a3

---
+++ Project/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ActionType = InputResponse.GamepadBinding.UpdateEvent;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void doAction(ActionType type, string action, float value)
+    {
+        Debug.Log(type + ": " + action + " " + value);
+    }
+
+}
