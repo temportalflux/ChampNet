faf1c9beea8184271c0a36a309f119ddd735e04f
dustin.yost <dustin.yost@mymail.champlain.edu>
2017-10-02 23:44:12 -0400

    Add new unity project
    Former-commit-id: 0f394392eb4420595a85860b9bd94a9eb62421e2

---
+++ Unity/Assets/Scripts/ChampNet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace ChampNetPlugin
+{
+
+    public class Network
+    {
+
+        const string IDENTIFIER = "ChampNetPlugin";
+
+        [DllImport(IDENTIFIER)]
+        public static extern int Create();
+
+        [DllImport(IDENTIFIER)]
+        public static extern int Destroy();
+
+        [DllImport(IDENTIFIER)]
+        public static extern int GetRandom();
+
+    }
+
+}
---
+++ Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ActionType = InputResponse.GamepadBinding.UpdateEvent;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ChampNetPlugin.Network.Create();
+	}
+
+    void OnDestroy()
+    {
+        ChampNetPlugin.Network.Destroy();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void doAction(ActionType type, string action, float value)
+    {
+        //Debug.Log(type + ": " + action + " " + value);
+        Debug.Log(ChampNetPlugin.Network.GetRandom() % 1000);
+    }
+
+}
