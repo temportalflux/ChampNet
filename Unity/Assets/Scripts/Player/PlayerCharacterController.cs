8863b15d45fb3907eef219e7138ca41ea9affbe2
jakedruth <jacob.ruth@mymail.champlain.edu>
2017-10-17 19:08:50 -0400

    added in the tile map (i do not like it at all) and the basic player input. not great but i will work on it more!
    Former-commit-id: ae0b3b6fb825fb64abdeadc95ed5bc4018693830

---
+++ Unity/Assets/Scripts/Player/PlayerCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [Header("Transform Dependencies")]
+    public Transform spriteTransform;
+
+    [Header("Player Character Controller values")]
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+
+    public void Move(Vector3 input)
+    {
+        transform.position += input * speed * Time.deltaTime;
+
+        if (input.sqrMagnitude > 0)
+        {
+            float alpha = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
+
+            spriteTransform.rotation = Quaternion.Euler(0, 0, alpha);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerCharacterController))]
+public class PlayerInputController : MonoBehaviour
+{
+    private PlayerCharacterController _pcc;
+
+
+    // Use this for initialization
+	void Start ()
+	{
+	    _pcc = GetComponent<PlayerCharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		Vector3 input = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+            input.x = -1;
+        else if (Input.GetKey(KeyCode.D))
+            input.x = 1;
+        else if (Input.GetKey(KeyCode.S))
+            input.y = -1;
+        else if (Input.GetKey(KeyCode.W))
+            input.y = 1;
+
+        input.Normalize();
+
+        _pcc.Move(input);
+
+    }
+}
