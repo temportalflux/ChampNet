19c703317d21b748b0cd64a54db6ae5d3ed55cab
dustin.yost <dustin.yost@mymail.champlain.edu>
2017-10-20 19:49:34 -0400

    Fix brush issues with misinterpretting directions
    Former-commit-id: f0290c4bca9e60ff84ba0156346aa157720bc1c4

--- Unity/Assets/TileMap/Brushes/Editor/BrushPath.cs
+++ Unity/Assets/TileMap/Brushes/Editor/BrushPath.cs
@@ -181,6 +181,7 @@ namespace UnityEditor
 
         public bool isTile(TileBase tile)
         {
+            if (tile == null) return false;
             foreach (PathItem item in this.tiles)
             {
                 if (item.tile == tile) return true;
@@ -205,7 +206,8 @@ namespace UnityEditor
 
         public delegate void DoPaint(Vector3Int pos, TileBase tile);
 
-        public TileSide getTileFor(Tilemap tilemap, Vector3Int position, List<TileSide> forceConnected)
+        public TileSide getTileFor(Tilemap tilemap, Vector3Int position, List<TileSide> forceConnected,
+            System.Func<TileSide, TileSide> translateSideToGlboal)
         {
             bool[] hasConnection = new bool[4];
             System.Action<TileSide, bool> markConnected = (TileSide side, bool connected) => { hasConnection[(int)side - 1] = connected; };
@@ -215,9 +217,15 @@ namespace UnityEditor
             System.Func<TileSide, TileSide, Vector3Int, bool> isSideFromPos =
                 (TileSide dir, TileSide key, Vector3Int pos) =>
                 {
+                    TileBase tilemapTile = tilemap.GetTile(offset(pos, dir));
+                    if (forceConnected.Contains(dir) && !this.isTile(tilemapTile))
+                    {
+                        tilemapTile = this.at(translateSideToGlboal == null ? dir : translateSideToGlboal(dir)).tile;
+                    }
+                    
                     // Offset position by direction, and get the tile
                     // compare against the config for some side
-                    return tilemap.GetTile(offset(pos, dir)) == this.at(key).tile;
+                    return tilemapTile == this.at(key).tile;
                 };
             System.Func<TileSide, TileSide, bool> isSide =
                 (TileSide direction, TileSide key) =>
@@ -233,9 +241,8 @@ namespace UnityEditor
             {
                 // Check to see if the tile at the position+direction is a valid tile
                 markConnected(cardinal,
-                    forceConnected != null && forceConnected.Contains(cardinal)
-                    ? true
-                    : this.isTile(tilemap.GetTile(offset(position, cardinal)))
+                    (forceConnected != null && forceConnected.Contains(cardinal)) ||
+                    this.isTile(tilemap.GetTile(offset(position, cardinal)))
                 );
                 if (isConnected(cardinal))
                 {
@@ -249,19 +256,31 @@ namespace UnityEditor
             // If all directions have a valid tile
             if (count >= 4)
             {
-                if (isSide(TileSide.NORTH, TileSide.EAST) && isSide(TileSide.EAST, TileSide.NORTH))
+                if (
+                    (isSide(TileSide.NORTH, TileSide.EAST) || isSide(TileSide.NORTH, TileSide.INTER_NE)) &&
+                    (isSide(TileSide.EAST, TileSide.NORTH) || isSide(TileSide.EAST, TileSide.INTER_NE))
+                    )
                 {
                     thisTileType = TileSide.ANTI_NE;
                 }
-                else if (isSide(TileSide.NORTH, TileSide.WEST) && isSide(TileSide.WEST, TileSide.NORTH))
+                else if (
+                    (isSide(TileSide.NORTH, TileSide.WEST) || isSide(TileSide.NORTH, TileSide.INTER_NW)) &&
+                    (isSide(TileSide.WEST, TileSide.NORTH) || isSide(TileSide.WEST, TileSide.INTER_NW))
+                    )
                 {
                     thisTileType = TileSide.ANTI_NW;
                 }
-                else if (isSide(TileSide.SOUTH, TileSide.EAST) && isSide(TileSide.EAST, TileSide.SOUTH))
+                else if (
+                    (isSide(TileSide.SOUTH, TileSide.EAST) || isSide(TileSide.SOUTH, TileSide.INTER_SE)) &&
+                    (isSide(TileSide.EAST, TileSide.SOUTH) || isSide(TileSide.EAST, TileSide.INTER_SE))
+                    )
                 {
                     thisTileType = TileSide.ANTI_SE;
                 }
-                else if (isSide(TileSide.SOUTH, TileSide.WEST) && isSide(TileSide.WEST, TileSide.SOUTH))
+                else if (
+                    (isSide(TileSide.SOUTH, TileSide.WEST) || isSide(TileSide.SOUTH, TileSide.INTER_SW)) &&
+                    (isSide(TileSide.WEST, TileSide.SOUTH) || isSide(TileSide.WEST, TileSide.INTER_SW))
+                )
                 {
                     thisTileType = TileSide.ANTI_SW;
                 }
@@ -345,21 +364,109 @@ namespace UnityEditor
             Vector3Int position, Tilemap tilemap, DoPaint doPaint, PathItem[] tilesIn = null)
         {
             PathItem[] tilesOut = newTileList(tilesIn, true);
-
-            // Cache the center tile to be placed
-            TileBase tilesCenter = this.at(TileSide.CENTER).tile;
-            TileBase tilesNorth = this.at(TileSide.NORTH).tile;
-
-            //Debug.Log("Connections: " + count);
-
-            TileSide thisTileType = this.getTileFor(tilemap, position, null);
             
+            List<TileSide> overrides = new List<TileSide>();
+
+            overrides.Clear();
+            foreach (TileSide cardinal in TILE_SIDE_CARDINAL)
+                overrides.Add(cardinal);
+            TileSide thisTileType = this.getTileFor(tilemap, position, overrides, null);
+
+            Vector3Int pos;
             foreach (TileSide cardinal in TILE_SIDE_CARDINAL)
             {
-                List<TileSide> overrides = new List<TileSide>();
+                overrides.Clear();
+                pos = offset(position, cardinal);
                 overrides.Add(opposite(cardinal));
-                TileSide tileType = this.getTileFor(tilemap, offset(position, cardinal), overrides);
-                this.set(cardinal, this.at(tileType).tile, ref tilesOut);
+                switch (cardinal)
+                {
+                    case TileSide.NORTH:
+                        overrides.Add(TileSide.EAST);
+                        overrides.Add(TileSide.WEST);
+                        break;
+                    case TileSide.EAST:
+                        overrides.Add(TileSide.NORTH);
+                        overrides.Add(TileSide.SOUTH);
+                        break;
+                    case TileSide.SOUTH:
+                        overrides.Add(TileSide.EAST);
+                        overrides.Add(TileSide.WEST);
+                        break;
+                    case TileSide.WEST:
+                        overrides.Add(TileSide.NORTH);
+                        overrides.Add(TileSide.SOUTH);
+                        break;
+                    default:
+                        continue;
+                }
+                TileSide tile = this.getTileFor(tilemap, pos, overrides,
+                    (TileSide localSide) =>
+                    {
+                        if (cardinal == TileSide.NORTH)
+                        {
+                            if (localSide == TileSide.WEST) return TileSide.INTER_NW;
+                            if (localSide == TileSide.EAST) return TileSide.INTER_NE;
+                        }
+                        else if (cardinal == TileSide.EAST)
+                        {
+                            if (localSide == TileSide.NORTH) return TileSide.INTER_NE;
+                            if (localSide == TileSide.SOUTH) return TileSide.INTER_SE;
+                        }
+                        else if (cardinal == TileSide.SOUTH)
+                        {
+                            if (localSide == TileSide.WEST) return TileSide.INTER_SW;
+                            if (localSide == TileSide.EAST) return TileSide.INTER_SE;
+                        }
+                        else if (cardinal == TileSide.WEST)
+                        {
+                            if (localSide == TileSide.NORTH) return TileSide.INTER_NW;
+                            if (localSide == TileSide.SOUTH) return TileSide.INTER_SW;
+                        }
+                        return localSide;
+                    }
+                );
+                this.set(cardinal, this.at(tile).tile, ref tilesOut);
+            }
+            foreach (TileSide corner in TILE_SIDE_INTER)
+            {
+                overrides.Clear();
+                pos = position;
+                switch (corner)
+                {
+                    case TileSide.INTER_NW:
+                        pos = offset(pos, TileSide.NORTH);
+                        pos = offset(pos, TileSide.WEST);
+                        overrides.Add(TileSide.SOUTH);
+                        overrides.Add(TileSide.EAST);
+                        break;
+                    case TileSide.INTER_NE:
+                        pos = offset(pos, TileSide.NORTH);
+                        pos = offset(pos, TileSide.EAST);
+                        overrides.Add(TileSide.SOUTH);
+                        overrides.Add(TileSide.WEST);
+                        break;
+                    case TileSide.INTER_SE:
+                        pos = offset(pos, TileSide.SOUTH);
+                        pos = offset(pos, TileSide.EAST);
+                        overrides.Add(TileSide.NORTH);
+                        overrides.Add(TileSide.WEST);
+                        break;
+                    case TileSide.INTER_SW:
+                        pos = offset(pos, TileSide.SOUTH);
+                        pos = offset(pos, TileSide.WEST);
+                        overrides.Add(TileSide.NORTH);
+                        overrides.Add(TileSide.EAST);
+                        break;
+                    default:
+                        continue;
+                }
+                TileSide tile = this.getTileFor(tilemap, pos, overrides,
+                    (TileSide localSide) =>
+                    {
+                        return localSide;
+                    }
+                );
+                this.set(corner, this.at(tile).tile, ref tilesOut);
             }
 
             this.set(TileSide.CENTER, this.at(thisTileType).tile, ref tilesOut);
