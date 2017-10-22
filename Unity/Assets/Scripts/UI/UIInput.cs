2f20e2346413b55d739eb3f8e8bdb0bbc44cd868
christopher.brennan <christopher.brennan@mymail.champlain.edu>
2017-10-22 13:56:37 -0400

    traverse both columns of the menu.
    Former-commit-id: a55265864230ed1ce5da9fda5e9fcb6f612ab35c

--- Unity/Assets/Scripts/UI/UIInput.cs
+++ Unity/Assets/Scripts/UI/UIInput.cs
@@ -8,8 +8,10 @@ public class UIInput : MonoBehaviour {
     public UnityEngine.UI.Button[] fields;
     public RectTransform selector;
 
+    public int numberOfColumns = 1;
+
     private int selected;
-    
+
 	void Start ()
     {
 
@@ -39,7 +41,7 @@ public class UIInput : MonoBehaviour {
                 case MappedAxis.Vertical:
                     
                     // Add value to current selection
-                    this.selected = (this.selected - (int)value);
+                    this.selected = (this.selected - ((int)value) * numberOfColumns);
                     // Ensure yield is negative
                     if (this.selected < 0) this.selected += this.fields.Length;
                     // Find remainder
@@ -47,6 +49,28 @@ public class UIInput : MonoBehaviour {
 
                     this.selector.position = new Vector3(this.selector.position.x, this.fields[this.selected].gameObject.transform.position.y, 0);
                     
+                    break;
+                case MappedAxis.Horizontal:
+
+                    // Add value to current selection
+                    this.selected = (this.selected - (int)value);
+                    // Ensure yield is negative
+                    if (this.selected < 0) this.selected += this.fields.Length;
+                    // Find remainder
+                    this.selected %= this.fields.Length;
+
+                    this.selector.position = new Vector3(this.selector.position.x, this.fields[this.selected].gameObject.transform.position.y, 0);
+
+
+                    if (this.selector.rotation.z == 0)
+                    {
+                        this.selector.localEulerAngles = new Vector3(this.selector.rotation.x, this.selector.rotation.y, 180);
+                    }
+                    else
+                    {
+                        this.selector.localEulerAngles = new Vector3(this.selector.rotation.x, this.selector.rotation.y, 0);
+                    }
+
                     break;
                 default:
                     break;
