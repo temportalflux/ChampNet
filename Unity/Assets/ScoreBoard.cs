c513fc6e6ddac4b14cc9deac9638b66c0ec857a5
christopher.brennan <christopher.brennan@mymail.champlain.edu>
2017-11-09 01:24:23 -0500

    started score board but need an explanation of network system
    Former-commit-id: a223b7500a1aa40d8a52dbf96cb017cdcf1c55ae

---
+++ Unity/Assets/ScoreBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour {
+
+    public GameObject GameManager_GameObject;
+    public GameManager GameManagerScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameManager_GameObject = GameObject.Find("GameManager");
+        GameManagerScript = GameManager_GameObject.GetComponent<GameManager>();
+
+        GameManagerScript.networkPlayerMap
+    }
+
+}
