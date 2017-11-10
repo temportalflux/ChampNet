21d003705472f334643d68b82c2c6c8c07d6ba4b
christopher.brennan <christopher.brennan@mymail.champlain.edu>
2017-11-09 19:07:59 -0500

    can now create text from inspector. Need to place them into correct location
    Former-commit-id: 1c0c9c671e094e7d9043c84b6640b53a026d3e5f

--- Unity/Assets/ScoreBoard.cs
+++ Unity/Assets/ScoreBoard.cs
@@ -6,30 +6,96 @@ using UnityEngine;
 public class ScoreBoard : MonoBehaviour {
 
     public GameObject GameManager_GameObject;
-    public GameManager GameManagerScript;
+    public GameManager GameManager_Script;
 
     private Text name;
     private Text win;
 
-	// Use this for initialization
-	void Start ()
+    private int currentText = 1;
+    // The listener class for handling all button effects
+    [System.Serializable]
+    public class RankText
     {
-        GameManager_GameObject = GameObject.Find("GameManager");
-        GameManagerScript = GameManager_GameObject.GetComponent<GameManager>();
+        [Tooltip("Rank setting (sets itself on start) (don't touch)")]
+        public int Rank;
+
+        [Tooltip("Name text created")]
+        public Text newName;
+
+        [Tooltip("Wins text created")]
+        public Text newWin;
+
+        [Tooltip("Size of font used for text created")]
+        public int fontSize;
+
+        [Tooltip("Style of the font used for text created")]
+        public FontStyle fontStyle;
+
+        [Tooltip("font of the font used for text created")]
+        public Font font;
     }
 
-    void Update()
+    [Tooltip("The list of listeners for buttons")]
+    public RankText[] rankText;
+
+
+    void Start ()
+    {
+        foreach(RankText text in rankText)
+        {
+            // set rank for later use
+            text.Rank = currentText;
+
+            // create new game object
+            GameObject newNameObject = new GameObject(("PlayerName (" + currentText + ")"), typeof(RectTransform)); 
+            // add text component to the new game object
+            var newNameText = newNameObject.AddComponent<Text>(); 
+
+            // set text info
+            newNameText.text = ("Empty rank: " + currentText );
+            // set alignment for RectTransform
+            newNameText.alignment = TextAnchor.MiddleCenter;
+            // set font size to what we wanted
+            newNameText.fontSize = text.fontSize;
+            // set font style
+            newNameText.fontStyle = text.fontStyle;
+            newNameText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font; // place holder unitl I can find how to add it from inspector
+
+            newNameText.transform.SetParent(transform);
+
+            // repeate above for wins
+
+            GameObject newWinObject = new GameObject(("PlayerWins (" + currentText + ")"), typeof(RectTransform));
+            var newWinText = newWinObject.AddComponent<Text>();
+
+            // set this to N/A as their is no one there to claim a win
+            newWinText.text = "N/A";
+            newWinText.alignment = TextAnchor.MiddleCenter;
+            newWinText.fontSize = text.fontSize;
+            newWinText.fontStyle = text.fontStyle;
+            newWinText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font; // place holder unitl I can find how to add it from inspector
+
+            newWinText.transform.SetParent(transform);
+
+            currentText += 1;
+        }
+        GameManager_GameObject = GameObject.Find("GameManager");
+        GameManager_Script = GameManager_GameObject.GetComponent<GameManager>();
+    }
+
+    void DisplayScoreBoard()
     {
         Debug.Log("yo it me, da score Board!");
 
-        for (int i = 1; i <= GameManagerScript.getSize() + 1; ++i)
+
+        foreach (RankText textNames in rankText)
         {
-            this.name = GameObject.Find("PlayerName (" + i + ")").GetComponent<Text>();
+            this.name = GameObject.Find("PlayerName (" + textNames.Rank + ")").GetComponent<Text>();
             {
                 this.name.text = "hi";
             }
 
-            if (this.win = GameObject.Find("PlayerWins (" + i + ")").GetComponent<Text>())
+            if (this.win = GameObject.Find("PlayerWins (" + textNames.Rank + ")").GetComponent<Text>())
             {
                 this.win.text = "yo";
             }
