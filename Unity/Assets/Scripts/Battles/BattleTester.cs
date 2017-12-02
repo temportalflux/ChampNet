a19e2fc21a935ec30bc2b126ded7d34d0af5b9df
CHAMPLAIN\jacob.ruth <@mymail.champlain.edu>
2017-12-02 15:48:04 -0500

    started work on the battles again. Lets do this bois
    Former-commit-id: 08f7837eb5c6b207f92481aa3e781c683247e72d

---
+++ Unity/Assets/Scripts/Battles/BattleTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Jake Ruth
+public class BattleTester : MonoBehaviour
+{
+    public BattleHandler battleHandler;
+    public BattleUIController battleUIController;
+
+    public KeeperSystem localPlayerTest;
+    public KeeperSystem otherPlayerTest;
+
+    [HideInInspector] public bool isBattleSetup;
+
+    void Start()
+    {
+        isBattleSetup = false;
+    }
+}
---
+++ Unity/Assets/Scripts/Battles/Editor/BattleTesterEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// Author: Jake Ruth
+[CustomEditor(typeof(BattleTester))]
+public class BattleTesterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BattleTester t = target as BattleTester;
+
+        GUILayout.Space(10f);
+
+        if (!t.isBattleSetup)
+        {
+            if (GUILayout.Button("Setup Battle Handler"))
+            {
+                if(t.localPlayerTest != null && t.otherPlayerTest != null)
+                {
+                    t.isBattleSetup = true;
+                }
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Button 1"))
+            {
+                
+            }
+
+            if (GUILayout.Button("Button 2"))
+            {
+
+            }
+
+            if (GUILayout.Button("Button 3"))
+            {
+
+            }
+
+            if (GUILayout.Button("Button 4"))
+            {
+
+            }
+
+            if (GUILayout.Button("Button 5"))
+            {
+
+            }
+
+            if (GUILayout.Button("Button 6"))
+            {
+
+            }
+
+            if (GUILayout.Button("Back"))
+            {
+
+            }
+        }
+    }
+}
