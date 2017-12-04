68318963e925ecb043bb16e5828bcc29f03fe0b8
dustin.yost <dustin.yost@mymail.champlain.edu>
2017-12-03 22:18:11 -0500

    Document GameManager

--- Unity/Assets/Scripts/Player/KeeperSystem.cs
+++ Unity/Assets/Scripts/Player/KeeperSystem.cs
@@ -9,29 +9,6 @@ using UnityEngine;
 /// <author>Jake Ruth</author>
 public class KeeperSystem : MonoBehaviour
 {
-
-    // TODO: Reference GameState
-
-    /// <summary>
-    /// The name of this player
-    /// </summary>
-    //public string keepername;
-
-    /// <summary>
-    /// The list of cretins to battle with
-    /// </summary>
-    // TODO: Move this to GameState.Player?
-    //public List<MonsterDataObject> monsters;
-
-    /// <summary>
-    /// The number of wins
-    /// </summary>
-    //public int wins;
-
-    /// <summary>
-    /// The number of losses
-    /// </summary>
-    //public int losses;
-
+    
 }
 
