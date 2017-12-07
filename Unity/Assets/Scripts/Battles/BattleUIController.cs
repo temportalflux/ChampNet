b3cf2d0a91e08c69d9495b30099577ac702bbc8a
CHAMPLAIN\jacob.ruth <@mymail.champlain.edu>
2017-12-07 04:53:59 -0500

    added the display for the cretin

--- Unity/Assets/Scripts/Battles/BattleUIController.cs
+++ Unity/Assets/Scripts/Battles/BattleUIController.cs
@@ -117,9 +117,12 @@ public class BattleUIController : MonoBehaviour
     public Text localCretinNameText;
     public Text localCretinHealthText;
     public Image localHPBar;
+    public Image localCretinImage;
+
     public Text otherCretinNameText;
     public Text otherCretinHealthText;
     public Image otherHPBar;
+    public Image otherCretinImage;
 
     void Start()
     {
@@ -216,10 +219,12 @@ public class BattleUIController : MonoBehaviour
         localCretinHealthText.text =
             string.Format("{0} / {1}", local.CurrentHP, local.maxHP);
         localHPBar.transform.localScale = new Vector3(local.CurrentHP / (float)local.maxHP, 1, 1);
+        localCretinImage.sprite = local.currentCretin.monsterStat.monsterPicture;
 
         otherCretinNameText.text = other.currentCretin.GetMonsterName;
         otherCretinHealthText.text =
             string.Format("{0} / {1}", other.CurrentHP, other.maxHP);
         otherHPBar.transform.localScale = new Vector3(other.CurrentHP / (float)other.maxHP, 1, 1);
+        otherCretinImage.sprite = other.currentCretin.monsterStat.monsterPicture;
     }
 }
